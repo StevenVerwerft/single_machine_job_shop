a815a139a2ba20735a421c973a7f1bbff58b528c
StevenVerwerft <steven.verwerft@student.uantwerpen.be>
2018-02-19 17:12:46 +0100

    Performance plotting module for plotting and comparing performance curves for different algorithms

--- performance_plotting.py
+++ performance_plotting.py
@@ -1,10 +1,14 @@
-import pandas as pd
+import sys
 import matplotlib.pyplot as plt
+import pandas as pd
+# Read in solutions from CLI
+argv = sys.argv
+solutions = [pd.read_csv(arg, sep=',', header=None, index_col=0, names=['solution']) for arg in argv if '.csv' in arg]
 
-results = pd.read_csv('test.csv', index_col=0)
-print(results)
+fig = plt.figure(figsize=(20, 10))
 
-results.plot()
-plt.xlabel('iteration')
-plt.ylabel('minimal maximal lateness')
+for sol in solutions:
+    print(sol.solution.values)
+    plt.plot(range(1, len(sol.solution.values)+1), sol.solution.values)
+
 plt.show()
