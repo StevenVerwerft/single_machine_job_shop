8f3e32e751d841b620a4901b7f8e9eeeeb4d0975
StevenVerwerft <steven.verwerft@student.uantwerpen.be>
2018-02-26 21:36:56 +0100

    finalized object oriented local search

--- classes.py
+++ classes.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
-import timeit
+import time
 import random
+import os
+import subprocess
+import matplotlib.pyplot as plt
+
 
 class Instance:
     """
@@ -86,7 +90,7 @@ class Instance2(Instance):
         super().__init__(self)
 
 
-class LocalSearch:
+# class LocalSearch:
 
     def __init__(self, max_iter):
         self.max_iter = max_iter
@@ -132,9 +136,12 @@ class JobSchedule:
         """Earliest Due Date Rule, sorts the jobschedule by earliest due date"""
         self.job_df = self.job_df.sort_values(by='d_date')
 
-    def calculate_goal(self):
+    def calculate_goal(self, job_df=None):
 
-        __job_df = self.job_df.__deepcopy__()
+        if job_df is not None:
+            __job_df = job_df
+        else:
+            __job_df = self.job_df.__deepcopy__()
 
         # 1. Calculate processing times for groups
         g_sequence = np.ones(self.n_jobs)  # Identifier for consequent group jobs
@@ -169,7 +176,8 @@ class JobSchedule:
     def swap_jobs(self, swap):
 
         __temp_job_df = self.job_df.__deepcopy__()
-        __temp_job_df.values[swap[0], :], __temp_job_df.values[swap[1], :] = self.job_df.values[swap[1], :]
+        __temp_job_df.iloc[swap[0], :], __temp_job_df.iloc[swap[1], :] = \
+            self.job_df.iloc[swap[1], :], self.job_df.iloc[swap[0], :]
 
         return __temp_job_df
 
@@ -186,27 +194,293 @@ class MovePool:
             for j in range(i+1, self.n_jobs):
                 self.move_pool.append((i, j))
 
-    def shuffle_movepool(self):
+    def move_shuffle(self):
         random.shuffle(self.move_pool)
+
+
+class Memory:
+
+    def __init__(self):
+        self.memory = []
+        self.current_length = len(self.memory)
+
+    def update_memory(self, item):
+        self.memory.append(item)
+        # could be done faster by writing += 1
+        self.current_length += 1
+
+    def remove_fifo(self):
+        self.memory.pop(0)
+        # could be done faster by writing -= 1
+        self.current_length = len(self.memory)
+
+    def clear_memory(self):
+
+        self.memory = []
+        self.current_length = 0
+
+
+class Solution:
+
+    def __init__(self, goalfunctionvalue=np.inf, move=(None, None), timestamp=np.inf):
+        self.goalvalue = goalfunctionvalue
+        self.move = move
+        self.timestamp = timestamp
+
+    def __str__(self):
+        return "({0}, {1}, {2})".format(self.goalvalue, self.move, self.timestamp)
+
+
+class SolutionMemory(Memory):
+
+    def __init__(self, max_length=None):
+        super().__init__()
+        self.max_length = max_length
+        self.best_solution = Solution(np.inf, (None, None))  # initialize by infinite goalfunction
+
+    def update_memory(self, solution=Solution()):
+        self.memory.append(solution)
+        if solution.goalvalue < self.best_solution.goalvalue:
+            self.best_solution = solution
+
+        self.current_length += 1
+
+    def last_solution(self):
+        return self.memory[-1]
+
+    def plot_memory(self, starttime):
+        import matplotlib.pyplot as plt
+        times = [solution.timestamp - starttime for solution in self.memory]
+        values = [solution.goalvalue for solution in self.memory]
+        plt.plot(times, values, label='solution space')
+        plt.legend()
+        plt.xlabel('runtime (s)')
+        plt.ylabel('goalfunction: maximal lateness')
+        plt.title('Local Search for minimal maximal lateness')
+
+        filepath = 'img/fig' + str(starttime) + '.png'
+        plt.savefig(filepath)
+        os.chmod(filepath, 777)
+        subprocess.Popen(['open ' + filepath], shell=True)
+
+
+class TabuList(Memory):
+
+    def __init__(self):
+        super().__init__()
 
 
 class LocalSearch:
 
-    def __init__(self, max_iter):
+    def __init__(self, jobschedule, max_iter, x_improvements, track_solution=True, EDD_rule=True):
+
+        self.instance = jobschedule  # jobschedule object
+        if EDD_rule:
+            self.instance.EDDRule()
+            self.instance.update_goal()
+        self.pool = MovePool(self.instance.job_df)
+        self.pool.generate_moves()
+
+        # metaparameters
         self.max_iter = max_iter
+        self.x_improvements = x_improvements
+        self.track_solution = track_solution
+        self.verbose = True
 
-    def evaluate_move(self, move, jobschedule, solution_memory):
+        # memory structures
+        self.solution_memory = SolutionMemory()
+        self.best_solution_memory = SolutionMemory()
+        self.first_x_memory = SolutionMemory(max_length=self.x_improvements)
 
-        local_job_df = jobschedule.swap_jobs(swap=move)
+        # start clock
+        self.starttime = time.time()
+        self.best_solution_memory.update_memory(solution=Solution(goalfunctionvalue=self.instance.goalvalue,
+                                                                  move=(None, None),
+                                                                  timestamp=self.starttime))
 
+    def solve(self, verbose=True):
+
+        for i in range(self.max_iter):
+            if verbose:
+                print("Iteration ", i)
+            local_optimum = True
+            improvement_found = False
+            iteration_memory = SolutionMemory()
+
+            for swap in self.pool.move_pool:
+
+                # Start search in local neighbourhood
+                ts = time.time()
+                goalval = self.instance.calculate_goal(self.instance.swap_jobs(swap=swap))
+                iteration_memory.update_memory(Solution(goalfunctionvalue=goalval,
+                                                        move=swap,
+                                                        timestamp=ts))
+
+                # Add swap evaluation to solution memory (could be inefficient)
+                if self.track_solution:
+                    self.solution_memory.update_memory(solution=Solution(goalfunctionvalue=goalval,
+                                                                         move=swap,
+                                                                         timestamp=ts))
+
+                # checks if local neighbour is better than last encountered solution
+                if goalval < self.best_solution_memory.last_solution().goalvalue:
+                    self.first_x_memory.update_memory(solution=Solution(goalfunctionvalue=goalval,
+                                                                        move=swap,
+                                                                        timestamp=ts))
+                    improvement_found = True
+                    if verbose:
+                        print("first x improving solutions:")
+                        for sol in self.first_x_memory.memory:
+                            print(sol)
+
+                if self.first_x_memory.current_length >= self.first_x_memory.max_length:
+                    final_solution = self.first_x_memory.best_solution
+                    if verbose:
+                        print("Max amount of improvements found")
+                        print("Best Improving Solution: \n", final_solution)
+                        print('breaking current local search...')
+
+                    self.instance.update_job_df(self.instance.swap_jobs(swap=swap))
+                    self.instance.update_goal()
+                    self.best_solution_memory.update_memory(solution=final_solution)
+
+                    # improvement found, no local optimum
+                    local_optimum = False
+                    self.first_x_memory.clear_memory()  # ready for next iteration
+                    break
+
+            if verbose:
+                print('All swaps evaluated...')
+
+            if improvement_found:
+                # If less than x improving moves are found in local neighbourhood
+                # Choose best solution from improving moves
+                final_solution = self.first_x_memory.best_solution
+                self.instance.update_job_df(self.instance.swap_jobs(final_solution.move))
+                self.instance.update_goal()
+
+                # update best solution memory
+                self.best_solution_memory.update_memory(solution=final_solution)
+
+                # improvement found, no local optimum
+                local_optimum = False
+                self.first_x_memory.clear_memory()  # ready for next iteration
+
+            # case of local otpimum
+            if local_optimum:
+                if verbose:
+                    print('No improving solutions are found, stuck in a local optimum')
+                    print('Best Global Solution encounterd: {}'.format(self.best_solution_memory.best_solution))
+                    print('Best Solution found in local neighbourhood: {}'.format(iteration_memory.best_solution))
+
+                swap = iteration_memory.best_solution.move
+                self.instance.update_job_df(self.instance.swap_jobs(swap=swap))
+                self.instance.update_goal()
+
+                self.best_solution_memory.update_memory(solution=iteration_memory.best_solution)
+                self.first_x_memory.clear_memory()  # ready for next iteration
+
+        if verbose:
+            print("Total runtime: ", time.time() - self.starttime)
+
+        self.best_solution_memory.plot_memory(self.starttime)
+
+
+def main2():
+    if True:
+        # Initialization
+        instance = JobSchedule('instance.csv')  # initiate job schedule object
+        instance.EDDRule()  # initialize earliest due date
+        instance.update_goal()
+        pool = MovePool(instance.job_df)  # initiate move pool object
+        pool.generate_moves()  # generate moves
+
+        # initialize memory structures
+        solution_memory = SolutionMemory()
+        best_solution_memory = SolutionMemory()
+        first_x_memory = SolutionMemory(max_length=15)
+        track_solution_memory = True
+
+        # basis for timestamp calculation
+        starttime = time.time()
+        best_solution_memory.update_memory(solution=Solution(instance.goalvalue, (None, None), starttime))
+
+    for i in range(10):
+        print('iteration ', i)
+        local_optimum = True
+        improvement_found = False
+        iteration_memory = SolutionMemory()
+
+        for swap in pool.move_pool:
+            # start search in local neighbourhood
+            ts = time.time()
+            # iterate over all neighbours
+            goalval = instance.calculate_goal(instance.swap_jobs(swap))
+            iteration_memory.update_memory(Solution(goalval, swap, ts))
+
+            # add the swap to the solution memory (inefficient)
+            if track_solution_memory:
+                solution_memory.update_memory(solution=Solution(goalval, swap, ts))
+
+            # checks if local neighbour is better than last encountered solution
+            if goalval < best_solution_memory.last_solution().goalvalue:
+                first_x_memory.update_memory(solution=Solution(goalval, swap, ts))
+                # best solution of solutions is updated
+                print('first x improving solutions: ')
+                for sol in first_x_memory.memory:
+                    print(sol)
+                improvement_found = True
+
+                if first_x_memory.current_length >= first_x_memory.max_length:
+                    print('max amount of improvements found')
+                    final_solution = first_x_memory.best_solution
+                    print('best solution found: \n', final_solution)
+                    instance.update_job_df(instance.swap_jobs(swap))
+                    instance.update_goal()
+
+                    # update best solution memory
+                    best_solution_memory.update_memory(solution=final_solution)
+
+                    local_optimum = False
+                    first_x_memory.clear_memory()  # ready for next iteration
+                    print('breaking current local search...')
+                    break  # breaks out local neighbourhood search
+
+        print('all swaps evaluated...')
+        if improvement_found:
+            final_solution = first_x_memory.best_solution
+            print('improving solution found: ', final_solution)
+            instance.update_job_df(instance.swap_jobs(final_solution.move))
+            instance.update_goal()
+
+            # update best solution memory
+            best_solution_memory.update_memory(solution=final_solution)
+
+            local_optimum = False
+            first_x_memory.clear_memory()  # ready for next iteration
+
+        # When reaching end of local search
+        if local_optimum:
+            print('No improving solution found, stuck in local optimum...')
+            print('Best solution found: {}'.format(solution_memory.best_solution.goalvalue))
+            print('Beste lokale oplossing: {}'.format(iteration_memory.best_solution))
+
+            swap = iteration_memory.best_solution.move
+            instance.update_job_df(instance.swap_jobs(swap))
+            instance.update_goal()
+
+            best_solution_memory.update_memory(solution=iteration_memory.best_solution)
+            first_x_memory.clear_memory()  # ready for next iteration
+
+    print('Total runtime: ', time.time() - starttime)
+    best_solution_memory.plot_memory(starttime)
 
 
 def main():
-    instance = JobSchedule('instance.csv')
-    pool = MovePool(instance.job_df)
-    pool.generate_moves()
-    pool.shuffle_movepool()
-    print(pool.move_pool)
+
+    instance = JobSchedule(pathname='instance.csv')
+    algorithm = LocalSearch(instance, max_iter=10, x_improvements=15)
+    algorithm.solve()
 
 
 if __name__ == '__main__':
