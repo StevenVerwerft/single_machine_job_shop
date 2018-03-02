9f6e1ed13a23015b415121b4f4e69c31b823045f
StevenVerwerft <steven.verwerft@student.uantwerpen.be>
2018-03-02 11:05:22 +0100

    fix 14: write program to plot multiple solution.csv files in one plot

--- plot_solution.py
+++ plot_solution.py
@@ -12,14 +12,28 @@ except:
     print("first argument should be the path to the solution!")
     quit()
 
-solution = pd.read_csv(path, index_col=0)
+try:
+    for path in args[1:]:
+        print(path)
+        solution = pd.read_csv(path, index_col=0)
 
-fig, ax = plt.subplots()
-ax.plot(solution.timestamps, solution.goalvalues, label='label not yet implemented')
-plt.xlabel('runtime (s)')
-plt.ylabel('Goal function: minimal maximal tardiness')
-plt.title('Local search for single machine job shop scheduling')
-plt.legend()
+        # add a label to the plot
+        label = 'No label found Dave...'
+        names = path.split('/')  # split the path name in its subdirectories
+        for name in names:
+            if 'instance' in name:
+                print()
+                label = name
+
+        # fig, ax = plt.subplots()
+        plt.plot(solution.timestamps, solution.goalvalues, label=label)
+        plt.xlabel('runtime (s)')
+        plt.ylabel('Goal function: minimal maximal tardiness')
+        plt.title('Local search for single machine job shop scheduling')
+        plt.legend()
+
+except:
+    print('I can\'t let you do that Dave...')
+
+
 plt.show()
-
-
